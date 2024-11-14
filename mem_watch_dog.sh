d174ce1c45bda8728b93949796e46bd529fa72e6
Christofer Tornkvist <jatorch@gmail.com>
2024-11-14 09:50:24 +0100

    memory watch dog kills pids

--- mem_watch_dog.sh
+++ mem_watch_dog.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
+# Memory watch dog
+# This script exists due to Firefox leaking memory. Thereof the application
+# filter only selects firefox threads to be killed at a too high memory usage.
+#
+
 # Configuration
-THRESHOLD_PERCENTAGE=80  # Percentage of memory usage to trigger alert
+FILTER_CMD=firefox-bin   # Kill only commands that match this pattern.
+N_PIDS_TO_KILL=10        # Number of Pids to kill.
+THRESHOLD_PERCENTAGE=90  # Percentage of memory usage to trigger killing
 INTERVAL=60              # Check interval in seconds
 EMAIL_TO="your_email@example.com"  # Recipient email address
 EMAIL_FROM="monitor@yourdomain.com" # Sender email address (configure properly)
 SUBJECT="High Memory Usage Alert"
+LOG_FILE=mem_watch_dog.log
+
 
 # Function to send email alert
 send_alert() {
@@ -18,16 +27,23 @@ check_memory() {
   used_mem=$(free -m | awk '/Mem:/ {print $3}')
   percentage=$(( (used_mem * 100) / total_mem ))
 
-  echo "Total Memory: $total_mem MB, Used Memory: $used_mem MB ($percentage%)"
+   if (( percentage >= THRESHOLD_PERCENTAGE )); then
+        echo "Memory usage exceeds threshold ($THRESHOLD_PERCENTAGE%)."
+        echo "Total Memory: $total_mem MB, Used Memory: $used_mem MB ($percentage%)" | tee $LOG_FILE
+        # Get processes consuming significant memory
+        PIDS=`ps aux --sort=-%mem | grep $FILTER_CMD | grep -v "grep" | head -n $N_PIDS_TO_KILL | awk '{print $2","$4","$1","$11}' | tee -a $LOG_FILE | sed 's/,.*$//g' | tr "\n" " "`
 
-  if (( percentage >= THRESHOLD_PERCENTAGE )); then
-    echo "Memory usage exceeds threshold ($THRESHOLD_PERCENTAGE%)."
-    # Get processes consuming significant memory
-    ps aux --sort=-%mem | head -n 15 | awk '{print $1","$2","$11}' | mail -s "$SUBJECT - Processes" "$EMAIL_TO" -r "$EMAIL_FROM"
-    send_alert
-  fi
+        for pid in "${PIDS}" ; do
+            kill -15 $pid
+            result=$?
+            echo "kill -15 $pid  returned: $result" | tee -a $LOG_FILE;
+        done
+        notify-send "Memory watch dog" "killed pids: $PIDS"
+   fi
 }
 
+# Main start
+rm -f $LOG_FILE
 
 # Main loop
 while true; do
